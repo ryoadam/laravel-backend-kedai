380ae5e7186672b887e2db33206d2016b3a2e7ab
ryoadam <ryoadam6@gmail.com>
2024-10-14 11:08:43 +0800

    git init laravel-backend-kedai

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('pages.dashboard', ['type_menu' => 'dashboard']);
+
+// });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+
+});
